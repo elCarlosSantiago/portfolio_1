492740531e4d024a2d67e731aad239b01c8abc05
CarlosSantiago Nevett <csnevett@gmail.com>
2021-08-19 15:18:17 -0700

    scroll implemented, about page bootstrapped

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div name="about" className="about">
+      <p>About</p>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,24 @@
+import { Link } from 'react-scroll';
+
 const Header = (props) => {
-
-
   return (
-    <header className='header'>
+    <header className="header">
       <nav>
-        <p>About</p>
-        <p>Skills</p>
-        <p>Projects</p>
-        <p>Contact</p>
+        <Link spy={true} smooth={true} to="about">
+          About
+        </Link>
+        <Link spy={true} smooth={true}>
+          Skills
+        </Link>
+        <Link spy={true} smooth={true}>
+          Projects
+        </Link>
+        <Link spy={true} smooth={true}>
+          Contact
+        </Link>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
