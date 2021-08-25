9af99c5d5c7be1ff16c6ecbfa4af2ba42c638fc7
CarlosSantiago Nevett <csnevett@gmail.com>
2021-08-25 09:25:19 -0700

    email js up

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,38 @@
+import emailjs from 'emailjs-com';
+import env from 'react-dotenv';
+
 const Contact = () => {
-  return(
+  function sendEmail(e) {
+    e.preventDefault();
+
+    const serviceId = env.EMAIL_SERVICE_ID;
+    const templateId = env.EMAIL_TEMPLATE_ID;
+    const emailUserId = env.EMAIL_USER_ID;
+
+    emailjs.sendForm(serviceId, templateId, e.target, emailUserId).then(
+      (result) => {
+        console.log('Email Sent', result.text);
+      },
+      (error) => {
+        console.log('Email Error', error.text);
+      }
+    );
+    e.target.reset();
+  }
+
+  return (
     <div className="contact">
-
+      <form className="contact-form" onSubmit={sendEmail}>
+        <input type="text" name="from_name" placeholder="Name" />
+        <input type="email" name="from_email" placeholder="example@email.com" />
+        <input type="text" name="subject" placeholder="Subject" />
+        <textarea placeholder="Your message" name="message" />
+        <button type="submit" value="Send">
+          Send Message
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
