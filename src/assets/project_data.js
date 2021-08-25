df3352dcbda6fd8a0e3c4a2bc3721b9656cdd5b2
CarlosSantiago Nevett <csnevett@gmail.com>
2021-08-24 18:44:52 -0700

    project component up and abstracted

---
+++ src/assets/project_data.js
@@ -0,0 +1,19 @@
+import mountain from '../assets/mountain.jpeg';
+export const project1 = {
+  project_id: 1,
+  profile: mountain,
+  title: 'Project Title 1',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit massa enim nec dui nunc mattis enim ut tellus. Egestas maecenas pharetra convallis posuere morbi leo.',
+  repoLink: 'https://www.google.com/',
+  deployLink: 'https://www.google.com/',
+};
+export const project2 = {
+  project_id: 2,
+  profile: mountain,
+  title: 'Project Title 2',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit massa enim nec dui nunc mattis enim ut tellus. Egestas maecenas pharetra convallis posuere morbi leo.',
+  repoLink: 'https://www.google.com/',
+  deployLink: 'https://www.google.com/',
+};
