4ad43c7ec228b07f27ccd7cb7bb4c48c3de5d062
CarlosSantiago Nevett <csnevett@gmail.com>
2021-08-25 12:00:57 -0700

    success and error messages up

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,11 @@
 import emailjs from 'emailjs-com';
+import { useEffect, useState } from 'react';
 import env from 'react-dotenv';
 
 const Contact = () => {
+  const [successSend, setSuccessSend] = useState(false);
+  const [error, setError] = useState(false);
+
   function sendEmail(e) {
     e.preventDefault();
 
@@ -12,21 +16,28 @@ const Contact = () => {
     emailjs.sendForm(serviceId, templateId, e.target, emailUserId).then(
       (result) => {
         console.log('Email Sent', result.text);
+        setSuccessSend(true);
       },
       (error) => {
         console.log('Email Error', error.text);
+        setError(true);
       }
     );
     e.target.reset();
   }
 
+
   return (
     <div className="contact">
+      <h2>Contact me</h2>
+
       <form className="contact-form" onSubmit={sendEmail}>
-        <input type="text" name="from_name" placeholder="Name" />
-        <input type="email" name="from_email" placeholder="example@email.com" />
+        <input type="text" name="from_name" placeholder="Name" className="from-name" />
+        <input type="email" name="from_email" placeholder="Email Address" />
         <input type="text" name="subject" placeholder="Subject" />
         <textarea placeholder="Your message" name="message" />
+        {successSend && <p>Your message was successful.</p>}
+        {error && <p>There was an error sending your message.</p>}
         <button type="submit" value="Send">
           Send Message
         </button>
