78fa2ce1108b6d9767ee1a147bedd08ef6a79336
CarlosSantiago Nevett <csnevett@gmail.com>
2021-09-01 11:37:46 -0700

    media queries

--- src/assets/project_data.js
+++ src/assets/project_data.js
@@ -4,7 +4,7 @@ export const project1 = {
   profile: mountain,
   title: 'Project Title 1',
   description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit massa enim nec dui nunc mattis enim ut tellus.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   repoLink: 'https://www.google.com/',
   deployLink: 'https://www.google.com/',
 };
@@ -13,7 +13,7 @@ export const project2 = {
   profile: mountain,
   title: 'Project Title 2',
   description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit massa enim nec dui nunc mattis enim ut tellus.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   repoLink: 'https://www.google.com/',
   deployLink: 'https://www.google.com/',
 };
